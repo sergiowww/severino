246956bad456163c539fd561829e874fa7472cde
U-MPT\sergio.eoliveira <sergiowww@gmail.com>
2016-08-02 10:10:55 -0300

    simplificacao de codigo

--- severino/src/main/java/br/mp/mpt/prt8/severino/controller/HomeController.java
+++ severino/src/main/java/br/mp/mpt/prt8/severino/controller/HomeController.java
@@ -1,10 +1,7 @@
 package br.mp.mpt.prt8.severino.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,9 +30,7 @@ public class HomeController {
 	 * @return
 	 */
 	@GetMapping("/")
-	public String inicio(HttpServletRequest request) {
-
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	public String inicio(Authentication auth) {
 		usuarioMediator.save(auth);
 		return "home";
 	}
