f6196f7d20542e0ecf7b8dae220f23d86fa547df
sergio.oliveira <sergio.oliveira@m62580.mpdft.gov.br>
2017-08-31 18:31:26 -0300

    remoção de código duplicado

--- severino/src/main/java/br/mp/mpt/prt8/severino/entity/Usuario.java
+++ severino/src/main/java/br/mp/mpt/prt8/severino/entity/Usuario.java
@@ -55,46 +55,6 @@ public class Usuario extends AbstractEntity<String> {
 		this.nome = nome;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof Usuario)) {
-			return false;
-		}
-		Usuario other = (Usuario) obj;
-		if (id == null) {
-			if (other.id != null) {
-				return false;
-			}
-		} else if (!id.equals(other.id)) {
-			return false;
-		}
-		return true;
-	}
-
 	public Local getLocal() {
 		return local;
 	}
