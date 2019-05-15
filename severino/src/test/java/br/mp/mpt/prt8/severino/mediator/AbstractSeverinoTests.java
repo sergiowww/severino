b9d6ef39dd925374e4ae2955bae66823ef6e7656
Sergio Eduardo Dantas de Oliveira <sergio.oliveira@mpdft.mp.br>
2019-05-15 20:03:05 -0300

    timezone testes

--- severino/src/test/java/br/mp/mpt/prt8/severino/mediator/AbstractSeverinoTests.java
+++ severino/src/test/java/br/mp/mpt/prt8/severino/mediator/AbstractSeverinoTests.java
@@ -2,18 +2,21 @@ package br.mp.mpt.prt8.severino.mediator;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.TimeZone;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Table;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import br.mp.mpt.prt8.severino.config.CesDbTestConfiguration;
 import br.mp.mpt.prt8.severino.mediator.carga.ICarga;
+import br.mp.mpt.prt8.severino.utils.Constantes;
 
 /**
  * Classe base de teste.
@@ -27,6 +30,11 @@ public abstract class AbstractSeverinoTests extends AbstractTransactionalJUnit4S
 
 	@PersistenceContext
 	protected EntityManager entityManager;
+
+	@BeforeClass
+	public static void configureTimeZone() {
+		TimeZone.setDefault(Constantes.DEFAULT_TIMEZONE);
+	}
 
 	@Before
 	public void setUpCarga() {
